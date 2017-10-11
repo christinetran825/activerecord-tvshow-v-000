d0e50d90b3d3e470c05ccea9d3e0e1a164580a9c
Christine Tran <christinetran825@gmail.com>
2017-10-11 22:41:05 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,11 +5,11 @@ class Show < ActiveRecord::Base
   end
 
   # def self.most_popular_show
-  #   self.highest_rating 
+  #   self.highest_rating
   # end
 
   def self.lowest_rating
-
+    minimum(:rating)
   end
 
   def self.least_popular_show
