27dd7bcbb1e0215639b64fced895f8a4ebdc62d0
Christine Tran <christinetran825@gmail.com>
2017-10-11 22:27:46 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,9 +1,31 @@
 class Show < ActiveRecord::Base
 
   def highest_rating
-    
+
   end
 
+  def highest_rating
 
+  end
+  
+  def highest_rating
+
+  end
+  
+  def highest_rating
+
+  end
+  
+  def highest_rating
+
+  end
+  
+  def highest_rating
+
+  end
+  
+  def highest_rating
+
+  end
 
 end
